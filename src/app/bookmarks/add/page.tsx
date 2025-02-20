4dd6f243f103fe78a3961f2f37d166b6fd4b0865
s2jung <didtpwjd6718@gmail.com>
2025-02-20 17:31:51 +0900

    SearchForm 컴포넌트 추가

--- src/app/bookmarks/add/page.tsx
+++ src/app/bookmarks/add/page.tsx
@@ -1,15 +1,49 @@
 'use client'
 
 import React, {useState} from "react";
+import { useRouter } from "next/navigation";
+import { saveBookmark } from "@/lib/bookmarkApi";
 
 const AddBookmarkPage = () => {
     const [title, setTitle] = useState("");
     const [url, setUrl] = useState("");
     const [message, setMessage] = useState<string|null>(null);
 
-    const handleSubmit = async (e: React.SyntheticEvent) => {
+    const router = useRouter();
 
-     }
+    const handleSubmit = async (e: React.SyntheticEvent) => {
+        e.preventDefault();
+
+        if(!url) {
+            alert("Please enter URL");
+            return;
+        }
+
+        if(!title) {
+          alert("Please enter Title");
+          return;
+	      }
+
+        const payload = {
+            title, url
+        }
+
+	    try {
+	      const response = await saveBookmark(payload);
+	      console.log("SaveBookmark response: ", response);
+	      setMessage("Bookmark saved successfully");  
+	      await router.push(`/bookmarks`);
+	      router.refresh();
+	    } catch (error) {
+	      console.log('등록 실패!!!!')  
+	      console.log(error);
+	      setMessage("Bookmark saved fail");  
+	    }
+	
+	    setTitle("");
+	    setUrl("");
+	        
+     } //handlesubmit
 
     return (
       <div>
