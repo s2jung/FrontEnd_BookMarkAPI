4049c2f8adc5b19bb35fba8ec82d96461d6c2828
s2jung <didtpwjd6718@gmail.com>
2025-02-20 15:52:43 +0900

    axios bookmarks list

--- src/app/bookmarks/page.tsx
+++ src/app/bookmarks/page.tsx
@@ -1,8 +1,21 @@
+import { getAllBookmarks } from "@/lib/bookmarkApi";
+import { BookmarkType, BookmarksResponse } from "@/types";
 
-export default function Home() {
+export default async function BookmarkPage()  {
+  const bookmarksData: Promise<BookmarksResponse> = getAllBookmarks()
+  const { data } = await bookmarksData
+  const bookmarks: BookmarkType[] = data
+
   return (
     <div>
-        <h1>Bookmarks Home</h1>
+        <h1>Welcome to Bookmarker</h1>
+        {
+          bookmarks.map(bookmark => {
+            return (
+                <h2 key={bookmark.id}>{bookmark.title}</h2>
+            )
+          })
+        }
     </div>
   );
 }
